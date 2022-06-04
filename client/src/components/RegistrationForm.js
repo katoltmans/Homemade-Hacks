e67f56367b1db9c266c6aeebd739b0e51b8765ce
katoltmans <katoltmans@msn.com>
2022-06-03 21:16:35 -0400

    Update RegistrationForm

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Box, Grid, Paper, TextField, Typography } from "@mui/material";
+import {
+    Box,
+    FormControl,
+    Grid,
+    Paper,
+    TextField,
+    Typography,
+} from "@mui/material";
 
 const RegistrationForm = () => {
     const navigate = useNavigate();
@@ -29,44 +36,78 @@ const RegistrationForm = () => {
     };
 
     return (
-        <Paper elevation={2}>
-            <Typography variant="h3" component="h1">
+        <Paper elevation={2} sx={{ p: 5, m: 5 }}>
+            <Typography variant="h3" component="h1" sx={{ mb: 3 }}>
                 Registration Form
             </Typography>
             <Box sx={{ flexGrow: 1 }}>
                 <Grid container spacing={3}>
-                    <Grid container item spacing={4}>
+                    <Grid container item spacing={1}>
                         <Grid item xs={6}>
-                            <TextField label="First Name" variant="outlined" />
+                            <TextField
+                                fullWidth
+                                label="First Name"
+                                variant="outlined"
+                            />
                         </Grid>
                         <Grid item xs={6}>
-                            <TextField label="First Name" variant="outlined" />
+                            <TextField
+                                fullWidth
+                                label="Last Name"
+                                variant="outlined"
+                            />
                         </Grid>
                     </Grid>
                     <Grid container item spacing={3}>
                         <Grid item xs={12}>
-                            <TextField label="First Name" variant="outlined" />
+                            <TextField
+                                fullWidth
+                                label="Email"
+                                variant="outlined"
+                            />
                         </Grid>
                     </Grid>
                     <Grid container item spacing={3}>
                         <Grid item xs={6}>
-                            <TextField label="First Name" variant="outlined" />
+                            <TextField
+                                fullWidth
+                                label="Birthdate"
+                                variant="outlined"
+                            />
                         </Grid>
                         <Grid item xs={6}>
-                            <TextField label="First Name" variant="outlined" />
+                            <TextField
+                                fullWidth
+                                label="Location"
+                                variant="outlined"
+                            />
                         </Grid>
                     </Grid>
                     <Grid container item spacing={3}>
                         <Grid item xs={6}>
-                            <TextField label="First Name" variant="outlined" />
+                            <TextField
+                                fullWidth
+                                label="Username"
+                                variant="outlined"
+                            />
                         </Grid>
                     </Grid>
                     <Grid container item spacing={3}>
                         <Grid item xs={6}>
-                            <TextField label="First Name" variant="outlined" />
+                            <TextField
+                                fullWidth
+                                type="password"
+                                label="Password"
+                                variant="outlined"
+                            />
                         </Grid>
                         <Grid item xs={6}>
-                            <TextField label="First Name" variant="outlined" />
+                            <TextField
+                                fullWidth
+                                type="password"
+                                label="Confirm Password"
+                                variant="outlined"
+                            />
                         </Grid>
                     </Grid>
                 </Grid>
