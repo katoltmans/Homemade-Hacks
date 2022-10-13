987e44604aa9b1b9b3fe98208d10a638ecc9d6f3
katoltmans <katoltmans@msn.com>
2022-10-13 16:56:05 -0400

    Initial Commit

---
+++ flask_app/client/src/components/CategoryPage.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+
+const CategoryPage = (props) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get("");
+    });
+};
+
+export default CategoryPage;
