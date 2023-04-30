e790cb2752300a6cb1a5e4d6a8bf80ce43c42bfb
katoltmans <katoltmans@msn.com>
2023-04-29 23:25:37 -0400

    Avoid circular references

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -1,9 +1,1 @@
-from flask import Flask
 from flask_app.controllers import users, hacks, base
-
-app = Flask(__name__,
-            static_url_path='/static', 
-            static_folder='/static',
-            template_folder='/templates')
-app.secret_key = "noneshallpass"
-app.logger.warning("FLASK_APP SUCCESS")
