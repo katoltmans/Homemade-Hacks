91fc4d7b1cc7662325a6bd0f6a2e5f97cd837c44
katoltmans <katoltmans@msn.com>
2023-04-29 21:52:50 -0400

    Fix imports and add log message for debugging

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from controllers import users
-from controllers import hacks
-from controllers import base
+from flask_app.controllers import users
+from flask_app.controllers import hacks
+from flask_app.controllers import base
 
 app = Flask(__name__,
             static_url_path='/static', 
             static_folder='/static',
             template_folder='/templates')
 app.secret_key = "noneshallpass"
+app.logger.warning("INIT SUCCESS")
