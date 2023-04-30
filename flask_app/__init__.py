c27b6cc7db8ed99058e2c1fc038435f8fc37b55a
katoltmans <katoltmans@msn.com>
2023-04-29 23:39:51 -0400

    Remove templates from the init because it is already correct automatically

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 app = Flask(__name__,
             static_url_path='/static', 
-            static_folder='/static',
-            template_folder='/templates')
+            static_folder='/static')
 app.secret_key = "noneshallpass"
 app.logger.warning("INIT SUCCESS")
