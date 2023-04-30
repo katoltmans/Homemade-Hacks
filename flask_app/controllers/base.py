b0089a861cce29f164fbae13db9690ee931517a7
katoltmans <katoltmans@msn.com>
2023-04-29 21:08:01 -0400

    Add a 404 error page

--- flask_app/controllers/base.py
+++ flask_app/controllers/base.py
@@ -5,4 +5,8 @@ from flask import render_template, redirect, request, session
 @app.route('/<path:path>')
 def display_base(path):
     return render_template("index.html")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
 #Source: https://codeburst.io/full-stack-single-page-application-with-vue-js-and-flask-b1e036315532
