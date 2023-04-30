2c551668207daefa96fd484d147f42fc27ce85b8
katoltmans <katoltmans@msn.com>
2023-04-29 21:46:54 -0400

    Add controllers to init and delete empty categories.py file

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from controllers import users
+from controllers import hacks
+from controllers import base
+
 app = Flask(__name__,
             static_url_path='/static', 
             static_folder='/static',
